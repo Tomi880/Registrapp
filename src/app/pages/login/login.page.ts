bfe2f1e7b4e20df12095c5eef4354dd977cc67f5
Gonzalo Fuentes <gonzalo.f.8c@gmail.com>
2022-10-04 22:50:17 -0300

    login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AnimationDirection, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  pageTitle = 'Login';
+
+  //Modelo
+  user: any = {
+    username : '',
+    password : ''
+  }
+
+
+  field : string = '';
+
+  constructor(private toastCtrl: ToastController, private route : Router) { }
 
   ngOnInit() {
+  }
+
+  ingresar(){
+    if(this.validateModel(this.user)){
+      this.presentToast(this.field);
+      
+    }
+    else{
+      this.presentToast("Falta: " + this.field);
+    }
+  }
+
+  validateModel (model: any){
+    for(var[key,value] of Object.entries(model)){
+      if (value == ''){
+        this.field = key;
+        return false;
+      }
+    }
+    if (value == 'admin'){
+      this.field = 'Bienvenido admin';
+      this.route.navigate(['/home']);
+      return true;
+    }
+    else{
+      this.field = 'Bienvenido Usuario';
+      this.route.navigate(['/home']);
+      return true;
+    }
+    
+  }
+
+
+  async presentToast(message: string, duration?: number){
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: duration?duration:2000
+    });
+    toast.present();
   }
 
 }
